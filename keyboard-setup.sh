4ee069e60ed1e0eb8133b98721757ab4d7c0480b
krvkir <krvkir@gmail.com>
2023-06-02 21:50:24 +0400

    [add] a script and an udev rule for linux to auto-configure kbd

---
+++ keyboard-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+user=krvkir
+userhome=$(getent passwd $user | cut -d: -f6)
+export XAUTHORITY="$userhome/.Xauthority"
+export DISPLAY=:0
+
+xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my $DISPLAY
