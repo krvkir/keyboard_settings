98a9b4b16587c1ed0aa3bed741205ea387956103
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-02-16 00:29:06 +0400

    [add] numbers and symbols layer

--- linux/geometry/autoupdate.sh
+++ linux/geometry/autoupdate.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
 inotifywait -q -m -e close_write custom | \
 while read -r filename event; do
-    xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my - | xkbprint - - | ps2pdf - > layout.pdf
+    # xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my - \
+    #     | xkbprint - - \
+    #     | ps2pdf - > layout.pdf
+    setxkbmap -layout us -geometry "custom(thinkpad_s1)" -print |\
+        xkbcomp -I"$HOME/.config/xkb" - - |\
+        xkbprint -label symbols -color -fit - - |\
+        ps2pdf - > layout.pdf
 done
