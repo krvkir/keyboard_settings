cbced7856d6a648bb71e12b4b93d56ff7932ff0d
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-03-15 02:23:04 +0400

    [add] wacom tablet initialization

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -6,3 +6,6 @@ export XAUTHORITY="$userhome/.Xauthority"
 #export DISPLAY=:0
 
 xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my $DISPLAY
+
+# setup wacom tablet
+xsetwacom --set $(xsetwacom --list devices | grep -i "TOUCH" | cut -f2 | cut -d" " -f2) "Area" "0 0 4560 2850"
