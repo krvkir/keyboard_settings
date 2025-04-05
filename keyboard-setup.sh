77f10ebf5caf4b5430b96e5422f10484971197fd
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-04-05 18:10:47 +0400

    [add] xmonad layer compatibility with xkb layer

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -5,6 +5,12 @@ userhome=$(getent passwd $user | cut -d: -f6)
 export XAUTHORITY="$userhome/.Xauthority"
 #export DISPLAY=:0
 
+# Make it systemd service (what those %i and %E mean?)
+#kmonad /home/$USER/repos/keyboard_settings/kmonad/main.kbd &
+#kmonad /home/$USER/repos/keyboard_settings/kmonad/zsa_moonlander.kbd &
+
+# Keep it for russian layout with special places for # & etc,
+# and also for yo and zsh letters.
 xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my $DISPLAY
 
 # setup wacom tablet
