509c7b41a8681f41103c23f04941bb4b177b2c6a
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-04-05 20:43:03 +0400

    [fix] ins->end

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -5,13 +5,11 @@ userhome=$(getent passwd $user | cut -d: -f6)
 export XAUTHORITY="$userhome/.Xauthority"
 #export DISPLAY=:0
 
-# Make it systemd service (what those %i and %E mean?)
-#kmonad /home/$USER/repos/keyboard_settings/kmonad/main.kbd &
-#kmonad /home/$USER/repos/keyboard_settings/kmonad/zsa_moonlander.kbd &
-
-# Keep it for russian layout with special places for # & etc,
-# and also for yo and zsh letters.
+# Keep xkb configs to have russian layout with special places for # & etc.
 xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my $DISPLAY
 
 # setup wacom tablet
-xsetwacom --set $(xsetwacom --list devices | grep -i "TOUCH" | cut -f2 | cut -d" " -f2) "Area" "0 0 4560 2850"
+TOUCH=$(xsetwacom --list devices | grep -i "TOUCH" | cut -f2 | cut -d" " -f2)
+xsetwacom --set $TOUCH "Area" "0 0 4560 2850"
+xsetwacom --set $TOUCH "TapTime" 100
+setup-wacom
