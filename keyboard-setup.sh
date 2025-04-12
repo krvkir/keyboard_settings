0e24668d5f578160aa3e427919a1506111b47ee6
Kirill Krasnoshchekov <krvkir@gmail.com>
2025-04-13 01:07:40 +0400

    [refactor] added `kk_` prefix to files to avoid conflists
    with system files residing in `/usr/share/X11/xkb`.

--- keyboard-setup.sh
+++ keyboard-setup.sh
@@ -6,10 +6,7 @@ export XAUTHORITY="$userhome/.Xauthority"
 #export DISPLAY=:0
 
 # Keep xkb configs to have russian layout with special places for # & etc.
-xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/my $DISPLAY
+xkbcomp -I"$HOME/.config/xkb" $HOME/.config/xkb/kk $DISPLAY
 
 # setup wacom tablet
-TOUCH=$(xsetwacom --list devices | grep -i "TOUCH" | cut -f2 | cut -d" " -f2)
-xsetwacom --set $TOUCH "Area" "0 0 4560 2850"
-xsetwacom --set $TOUCH "TapTime" 100
 setup-wacom
